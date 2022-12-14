5ff90145eaf5243421001c7aa84e7e6d7524b435
avs-abhishek123 <abhishek.avs123@gmail.com>
2022-12-15 00:38:18 +0530

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,10 @@
 import '../styles/globals.css'
 import '../styles/mode.scss'
+import '../styles/swipable-button.scss'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+      <Component {...pageProps} />
+  )
 }
